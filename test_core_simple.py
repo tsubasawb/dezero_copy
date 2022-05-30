bb912d6f413130ad8541e09e7b5ce0644c343afa
TsubasaImoto <tsubasa.imoto@gmail.com>
2022-05-30 21:07:46 +0900

    1st stage

---
+++ test_core_simple.py
@@ -0,0 +1,18 @@
+import unittest
+from core_simple import *
+
+class SquareTest(unittest.TestCase):
+    def test_forward(self):
+        x = Variable(np.array(2.0))
+        y = square(x)
+        expected = np.array(4.0)
+        self.assertEqual(y.data, expected)
+    
+    def test_backward(self):
+        x = Variable(np.array(3.0))
+        y = square(x)
+        y.backward()
+        expected = np.array(6.0)
+        self.assertEqual(x.grad, expected)
+
+unittest.main()
